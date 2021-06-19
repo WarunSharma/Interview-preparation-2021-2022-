573ae6170e41602c96339411622d3cd209b6b563
Warun Sharma <warunsharma.95@gmail.com>
2021-06-20 00:21:51 +0530

    Added optimal solution for 4Sum problem

---
+++ Data Structure & Algorithms/Array/4Sum/4Sum_Solution_Optimal.js
@@ -0,0 +1,76 @@
+
+//Time Complexity: O(n2logn) , Space Complexity: O(n) O(n)
+let fourSum = function(nums,target){
+
+    //If given array length is less
+    if(nums.length<4){
+        return [];
+    }
+
+    //Initialize resultant array
+    let result = [];
+
+    //Defining Map to store all pair sums.
+    let hashMap = new Map();
+
+    nums.sort((a,b)=>a-b);
+    //Storing sum as index and pairs indexs as values
+    for(let i=0;i<nums.length-1;++i){
+        for(let j=i+1;j<nums.length;++j){
+            let sum=nums[i]+nums[j];
+            let temp=[];
+            if(hashMap.has(sum)){
+                temp=hashMap.get(sum);
+                temp.push([i,j])
+            }
+            else{
+                temp.push([i,j]);
+            }
+
+            hashMap.set(sum,temp);
+        }
+    }
+
+    //Defining a set for excluding duplicate quadruplets
+    let set = new Set();
+    for(let i=0;i<nums.length-1;++i){
+        //Skipping duplicate elements
+        if(i>0 && nums[i]==nums[i-1]){
+            continue;
+        }
+        for(let j=i+1;j<nums.length;++j){
+            //Skipping duplicate elements
+            if(j>i+1 && nums[j]==nums[j-1]){
+                continue;
+            }
+
+            //Get leftover sum
+            let leftSum = target - (nums[i]+nums[j]);
+
+            //Get arr pairs with leftover sum
+            let arr = hashMap.get(leftSum);
+
+            for(let k=0;k<arr.length;++k){
+                if(j<arr[k][0] && j<arr[k][1]){
+                    let str=''+nums[i]+nums[j]+nums[arr[k][0]]+nums[arr[k][1]];
+                    if(set.has(str)){
+                        continue;
+                    }
+                    result.push([nums[i],nums[j],nums[arr[k][0]],nums[arr[k][1]]])
+                    set.add(str)
+                }
+            }
+        }
+    }
+
+    return result;
+}
+
+console.log('Testcase1')
+console.log(fourSum([1,0,-1,0,-2,2],0));
+
+console.log('Testcase2')
+console.log(fourSum([2,2,2,2,2],8));
+
+console.log('Testcase3')
+console.log(fourSum([-1,0,1,0,-1,1],0));
---
+++ Data Structure & Algorithms/Array/4Sum/Refer_Solution_Here.md
@@ -0,0 +1,6 @@
+
+Please refer this video for reference https://www.youtube.com/watch?v=8ViERnSgPKs
+
+Or 
+
+Cantact me.

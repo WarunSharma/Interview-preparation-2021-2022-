cc1618e9a64534987a66f6ecb72158a3cc49c384
Warun Sharma <warunsharma.95@gmail.com>
2021-06-20 15:50:16 +0530

    Added comments in Trapping Rain Water problem

--- Data Structure & Algorithms/Array/Trapping_Rain_Water/TrappingRainWater_Solution.js
+++ Data Structure & Algorithms/Array/Trapping_Rain_Water/TrappingRainWater_Solution.js
@@ -3,18 +3,27 @@ let trappingRainWater = function(blocks){
 
     let waterStored=0;
 
+    //blocks length less than 3. No place to store water
+    if(blocks.length<3){
+        return waterStored;
+    }
+
+    //Iterating blocks between 1st and last
     for(let i=1;i<blocks.length-1;++i){
 
+        //Calculating leftmost maximum element
         let leftMax=blocks[i-1];
         for(let j=i-1;j>=0;--j){
             leftMax=Math.max(blocks[j],leftMax);
         }
 
+        //Calculating rightmost maximum element
         let rightMax=blocks[i+1];
         for(let k=i+1;k<blocks.length;++k){
             rightMax=Math.max(blocks[k],rightMax);
         }
 
+        //Calculate minimum of leftmax, rightmax and subtract ith element from blocks
         if(Math.min(leftMax,rightMax)-blocks[i]>0){
             waterStored+=Math.min(leftMax,rightMax)-blocks[i];
         }

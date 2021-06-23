7caf5d919ee78542469e136e02958e90d7c287ef
Warun Sharma <warunsharma.95@gmail.com>
2021-06-23 19:30:06 +0530

    Added solution for problem Majority Element II

---
+++ Data Structure & Algorithms/Array/Majority_ElementII/MajorityElementII_Solution.js
@@ -0,0 +1,36 @@
+
+let majorityElementII = function(nums){
+    let sortedNums= nums.sort((a,b)=>a-b);
+
+    let count=1;
+    let res=[];
+    for(let i=0;i<sortedNums.length-1;++i){
+        for(let j=i+1;j<sortedNums.length;++j){
+            if(sortedNums[i]==sortedNums[j]){
+                count++;
+            }
+            else{
+                if(count>Math.floor(sortedNums.length/3)){
+                    res.push(sortedNums[i]);
+                }
+                count=1;
+                break;
+            }
+        }
+    }
+    if(count>Math.floor(sortedNums.length/3)){
+        res.push(sortedNums[nums.length-1]);
+    }
+
+    return res;
+}
+
+
+console.log('Testcase1');
+console.log(majorityElementII([3,2,3]));
+
+console.log('Testcase1');
+console.log(majorityElementII([1]));
+
+console.log('Testcase1');
+console.log(majorityElementII([1,2]));

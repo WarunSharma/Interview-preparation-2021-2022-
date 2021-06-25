a15ef8bba34501145ad862b8602c558447b44393
Warun Sharma <warunsharma.95@gmail.com>
2021-06-25 23:20:29 +0530

    Added solution for problem Longest length of even odd subarray

---
+++ Data Structure & Algorithms/Array/Longest_Length_EvenOdd_SubArray/LongestLengthEvenOddSubArray_Solution.js
@@ -0,0 +1,30 @@
+
+/*
+* Time Complexity: O(n)
+* Space Complexity: O(1)
+*/
+
+let longestLengthEvenOddSubarray=function(nums){
+    let maxLength=1,curLength=1;
+    for(let i=1;i<nums.length;++i){
+        if((nums[i]%2==0 && nums[i-1]%2==1) || (nums[i]%2==1 && nums[i-1]%2==0)){
+            ++curLength;
+        }
+        else{
+            maxLength=Math.max(curLength,maxLength);
+            curLength=1;
+        }
+    }
+
+    maxLength=Math.max(curLength,maxLength);
+    return maxLength;
+}
+
+console.log('Testcase1');
+console.log(longestLengthEvenOddSubarray([1, 2, 3, 4, 5, 7, 9]));
+
+console.log('Testcase2');
+console.log(longestLengthEvenOddSubarray([1, 2, 3, 4, 5]));
+
+console.log('Testcase3');
+console.log(longestLengthEvenOddSubarray([1, 3, 5]));
---
+++ Data Structure & Algorithms/Array/Longest_Length_EvenOdd_SubArray/Problem_Statement.md
@@ -0,0 +1,18 @@
+
+https://www.geeksforgeeks.org/length-of-the-longest-alternating-even-odd-subarray/
+
+Length of the longest alternating even odd subarray
+Difficulty Level : Easy
+Last Updated : 08 Jun, 2021
+Given an array a[] of N integers, the task is to find the length of the longest Alternating Even Odd subarray present in the array. 
+Examples: 
+ 
+
+Input: a[] = {1, 2, 3, 4, 5, 7, 9} 
+Output: 5 
+Explanation: 
+The subarray {1, 2, 3, 4, 5} has alternating even and odd elements.
+Input: a[] = {1, 3, 5} 
+Output: 0 
+Explanation: 
+There is no such alternating sequence possible.

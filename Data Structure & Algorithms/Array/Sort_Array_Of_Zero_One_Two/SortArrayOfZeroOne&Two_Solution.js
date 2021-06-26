a64df8e2684cdf08d62bf47f8ac2dc379761432d
Warun Sharma <warunsharma.95@gmail.com>
2021-06-26 21:36:31 +0530

    Added solution for problrm Sort Array Of Zero, One & Two

---
+++ Data Structure & Algorithms/Array/Sort_Array_Of_Zero_One_Two/SortArrayOfZeroOne&Two_Solution.js
@@ -0,0 +1,43 @@
+
+/*
+* Time Complexity: O(n)
+* Space Complexity: O(1)
+*/
+
+let sortArrayOfZeroOnetwo = function(nums){
+    let countZero=0,countOne=0,countTwo=0;
+    for(let i=0;i<nums.length;++i){
+        if(nums[i]==0){
+            ++countZero;
+        }
+        else if(nums[i]==1){
+            ++countOne;
+        }
+        else{
+            ++countTwo;
+        }
+    }
+
+    for(let i=0;i<nums.length;++i){
+        if(countZero>0){
+            nums[i]=0;
+            countZero--;
+        }
+        else if(countOne>0){
+            nums[i]=1;
+            countOne--;
+        }
+        else{
+            nums[i]=2;
+            countTwo--;
+        }
+    }
+
+    return nums;
+}
+
+console.log('Testcase1');
+console.log(sortArrayOfZeroOnetwo([0 ,2 ,1 ,2 ,0]));
+
+console.log('Testcase2');
+console.log(sortArrayOfZeroOnetwo([0 ,1 , 0]));

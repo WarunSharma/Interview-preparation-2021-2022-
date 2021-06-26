fa2e34b1e382339651730ac4e85f6176929eb885
Warun Sharma <warunsharma.95@gmail.com>
2021-06-26 15:43:50 +0530

    Bug fix in optimal solution of problm smallest positive missing number

--- Data Structure & Algorithms/Array/Smallest_Positive_Missing_Number/SmallestPositiveMisingNumber_Solution_Optimal.js
+++ Data Structure & Algorithms/Array/Smallest_Positive_Missing_Number/SmallestPositiveMisingNumber_Solution_Optimal.js
@@ -6,34 +6,43 @@
 */
 
 let smallestPostiveNumber = function(nums){
+    
+    let l=nums.length;
     let temp;
-    let i=0;
-    while(i<nums.length){
-        if(nums[i]<nums.length-1 && nums[i]>=0 && nums[i]!=i){
-            temp=nums[nums[i]];
-            nums[nums[i]]=nums[i];
-            nums[i]=temp;
+    for(let i=0;i<l;){
+        if(nums[i]>l || nums[i]<=0 || nums[i]==i+1){
+            i++;
         }
         else{
-            ++i;
+            if(nums[nums[i]-1]==nums[i]){
+                ++i;
+            }
+            else{
+                temp=nums[nums[i]-1];
+                nums[nums[i]-1]=nums[i];
+                nums[i]=temp;
+            }
         }
     }
 
-    for(let i=0;i<nums.length;++i){
-        if(nums[i]!=i){
-            return i;
+    for(let i=0;i<l;++i){
+        if(nums[i]!=i+1){
+            return i+1;
         }
     }
 
-    return nums.length;
+    return l+1;
 
 }
 
 console.log('Testcase1');
-console.log(smallestPostiveNumber([0,-10,1,3,-20]))
+console.log(smallestPostiveNumber([0,-10,1,3,-20]));
 
 console.log('Testcase2');
-console.log(smallestPostiveNumber([5,4,3,2,1,0]))
+console.log(smallestPostiveNumber([5,4,3,2,1,0]));
 
 console.log('Testcase3');
-console.log(smallestPostiveNumber([1,2,3,4,5]))
+console.log(smallestPostiveNumber([1,2,3,4,5]));
+
+console.log('Testcase4');
+console.log(smallestPostiveNumber([1,1,2]))

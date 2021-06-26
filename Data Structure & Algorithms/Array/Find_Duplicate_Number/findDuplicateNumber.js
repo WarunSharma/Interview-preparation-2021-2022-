fff4de7ea7345614210d8b84f9463a4812fd9204
Warun Sharma <warunsharma.95@gmail.com>
2021-06-26 16:56:54 +0530

    Added solution for problem Find Duplicate Number

---
+++ Data Structure & Algorithms/Array/Find_Duplicate_Number/findDuplicateNumber.js
@@ -0,0 +1,31 @@
+
+/*
+* Time Complexity: O(nlogn)
+* Space Complexity: O(1)
+*/
+
+var findDuplicate = function(nums) {
+    
+    for(let i=0;i<nums.length;++i){
+        if(nums[Math.abs(nums[i])-1]<0){
+            return Math.abs(nums[i]);
+        }
+        else{
+            nums[Math.abs(nums[i])-1]=-1*nums[Math.abs(nums[i])-1];
+        }
+    }
+
+};
+
+
+console.log('Testcase1')
+console.log(findDuplicate([1,1,2]));
+
+console.log('Testcase2')
+console.log(findDuplicate([1,3,4,2,2]));
+
+console.log('Testcase1')
+console.log(findDuplicate([3,1,3,4,2]));
+
+console.log('Testcase1')
+console.log(findDuplicate([1,1]));

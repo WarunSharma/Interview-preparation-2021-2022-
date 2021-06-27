46527f924e8e3cc8c0510dc508832dba3ee09ea6
Warun Sharma <warunsharma.95@gmail.com>
2021-06-27 13:25:49 +0530

    Added solution for problem Union of Two Arrays

---
+++ Data Structure & Algorithms/Array/Union_Of_Two_Arrays/UnionOfTwoArrays_Solution.js
@@ -0,0 +1,28 @@
+
+/*
+* Time Complexity: O(n) or O(m)
+* Space Complexity: O(n+m)
+*/
+
+let unionOfTwoArray=function(nums1,nums2){
+    let set=new Set();
+
+    nums1.map(el=>{
+        set.add(el);
+    });
+
+    nums2.map(el=>{
+        set.add(el);
+    });
+
+    let res=[];
+
+    for(let i of set){
+        res.push(i);
+    }
+
+    return res;
+}
+
+console.log('Testcase1');
+console.log(unionOfTwoArray([1, 2, 3, 4, 5],[1, 2, 3]));

78382e19065eb5d991d1d3ca8297f3472aab1003
Warun Sharma <warunsharma.95@gmail.com>
2021-07-12 19:08:25 +0530

    Added solution for problem Detect a Loop in Singly Linked List

---
+++ Data Structure & Algorithms/Linked Lists/Detect_Loop_In_SinglyLinkedList/DetectLoopInSinglyLinkedList_Solution.js
@@ -0,0 +1,73 @@
+
+
+/* Insert an Element at End of Linked List
+* Time Complexity: O(n)
+* Space Complexity: O(n)
+*/
+
+function Node(value){
+    this.data=value;
+    this.next=null;
+}
+
+function SinglyLinkedList(value){
+    this.head=null;
+    this.loop=null;
+    this.size=0;
+}
+
+SinglyLinkedList.prototype.insert=function(value){
+    if(this.head==null){
+        this.head=new Node(value);
+    }
+    else{
+        let cursor=this.head;
+        while(cursor.next!=null){
+            cursor=cursor.next;
+        }
+        cursor.next=new Node(value)
+        if(value==3){
+            this.loop=cursor.next;
+        }
+    }
+    ++this.size;
+}
+
+SinglyLinkedList.prototype.display=function(){
+    let cursor=this.head;
+    while(cursor!=null){
+        console.log(cursor.data);
+        cursor=cursor.next;
+    }
+}
+
+SinglyLinkedList.prototype.createLoop=function(){
+    let cursor=this.head;
+    while(cursor.next!=null){
+        cursor=cursor.next;
+    }
+    cursor.next=this.loop;
+}
+
+SinglyLinkedList.prototype.detectLoop=function(){
+    let slow=this.head;
+    let fast=this.head.next;
+    while(slow!=null && fast.next!=null && fast.next.next!=null){
+        if(slow==fast)
+            return true;
+        slow=slow.next;
+        fast=fast.next.next;    
+    }
+
+    return false;
+}
+
+let SLL=new SinglyLinkedList();
+SLL.insert(1);
+SLL.insert(2);
+SLL.insert(3);
+SLL.insert(4);
+SLL.insert(5);
+SLL.createLoop();
+//SLL.display();
+console.log(SLL.detectLoop());

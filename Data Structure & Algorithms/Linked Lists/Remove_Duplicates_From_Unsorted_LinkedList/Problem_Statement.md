c145f5d8b4980c998500869ba2d42b27903edc2a
Warun Sharma <warunsharma.95@gmail.com>
2021-07-14 05:42:15 +0530

    Added solution for problem Remove Duplicates from Unsorted LinkedList

---
+++ Data Structure & Algorithms/Linked Lists/Remove_Duplicates_From_Unsorted_LinkedList/Problem_Statement.md
@@ -0,0 +1,8 @@
+
+https://www.geeksforgeeks.org/remove-duplicates-from-an-unsorted-linked-list/
+
+Remove duplicates from an unsorted linked list
+Difficulty Level : Easy
+Last Updated : 02 Jul, 2021
+Write a removeDuplicates() function that takes a list and deletes any duplicate nodes from the list. The list is not sorted. 
+For example if the linked list is 12->11->12->21->41->43->21 then removeDuplicates() should convert the list to 12->11->21->41->43. 
---
+++ Data Structure & Algorithms/Linked Lists/Remove_Duplicates_From_Unsorted_LinkedList/RemoveDuplicatesFromUnsortedLinkedList_Solution.js
@@ -0,0 +1,68 @@
+
+
+/* Insert an Element at End of Linked List
+* Time Complexity: O(n)
+* Space Complexity: O(n)
+*/
+
+function Node(value){
+    this.data=value;
+    this.next=null;
+}
+
+function SinglyLinkedList(value){
+    this.head=null;
+    this.size=0;
+}
+
+SinglyLinkedList.prototype.insert=function(value){
+    if(this.head==null){
+        this.head=new Node(value);
+    }
+    else{
+        let cursor=this.head;
+        while(cursor.next!=null){
+            cursor=cursor.next;
+        }
+        cursor.next=new Node(value)
+    }
+    ++this.size;
+}
+
+SinglyLinkedList.prototype.display=function(){
+    let cursor=this.head;
+    while(cursor!=null){
+        console.log(cursor.data);
+        cursor=cursor.next;
+    }
+}
+
+SinglyLinkedList.prototype.removeDuplicates=function(){
+    let curNode=this.head;
+    while(curNode!==null){
+        let curNextNode=curNode.next;
+        let prevNode=curNode;
+        while(curNextNode!==null){
+            if(curNode.data==curNextNode.data){
+                prevNode.next=curNextNode.next;
+                curNextNode=prevNode.next;
+            }
+            else{
+                prevNode=curNextNode;
+                curNextNode=curNextNode.next;
+            }
+
+        }
+        curNode=curNode.next;
+    }
+}
+
+let SLL=new SinglyLinkedList();
+SLL.insert(1);
+SLL.insert(2);
+SLL.insert(3);
+SLL.insert(2);
+SLL.insert(1);
+SLL.display();
+SLL.removeDuplicates();
+SLL.display();

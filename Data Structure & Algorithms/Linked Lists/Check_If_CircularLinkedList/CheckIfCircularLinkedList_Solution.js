6d2d93ac877dd0e42f750fb4f6331342c62eaed4
Warun Sharma <warunsharma.95@gmail.com>
2021-07-17 15:25:56 +0530

    Added solution for problem Check If CircularLinkedList

---
+++ Data Structure & Algorithms/Linked Lists/Check_If_CircularLinkedList/CheckIfCircularLinkedList_Solution.js
@@ -0,0 +1,54 @@
+
+function Node(value){
+    this.data=value;
+    this.next=null;
+}
+
+function CircularLinkedList(){
+    this.head=null;
+    this.size=0;
+}
+
+CircularLinkedList.prototype.insert=function(value){
+    if(this.head==null){
+        this.head=new Node(value);
+        this.head.next=this.head;
+    }
+    else{
+        let cursor=this.head;
+        while(cursor.next!=this.head){
+            cursor=cursor.next;
+        }
+
+        cursor.next=new Node(value);
+        cursor.next.next=this.head
+    }
+}
+
+CircularLinkedList.prototype.display=function(){
+    let cursor=this.head;
+    do{
+        console.log(cursor.data);
+        cursor=cursor.next;
+    }while(cursor!=this.head)
+}
+
+CircularLinkedList.prototype.isCircular=function(){
+    if(this.head===null){
+        return true;
+    }
+    let node=this.head.next;
+    while(node!==null && node!==this.head){
+        node=node.next;
+    }
+    return this.head===node
+}
+
+let CLL=new CircularLinkedList();
+CLL.insert(1);
+CLL.insert(2);
+CLL.insert(3);
+CLL.insert(4);
+CLL.insert(5);
+CLL.display()
+console.log(CLL.isCircular());

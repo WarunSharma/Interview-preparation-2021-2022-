053bf041e09245dea7c7cbd51d6b62fbebbd0f15
Warun Sharma <warunsharma.95@gmail.com>
2021-07-27 21:50:40 +0530

    Added comments in Find Pair with given difference

--- Data Structure & Algorithms/Searching&Sorting/Find_Pair_Given_Difference/FindPairGivenDifference_Solution.js
+++ Data Structure & Algorithms/Searching&Sorting/Find_Pair_Given_Difference/FindPairGivenDifference_Solution.js
@@ -1,29 +1,45 @@
 
-function findPairs(nums,k){
-    let low=0,high=1;
-    nums=nums.sort((a,b)=>a-b);
+/*
+*
+* Time Complexity: O(n)
+* Space Complexity: O(1)
+*
+* @param nums Given array
+* @param k Given Difference
+* @returns boolean true or false
+*
+*/
+
+
+function findPairs(nums, k) {
+    let low = 0, high = 1;
+    nums = nums.sort((a, b) => a - b);
     console.log(nums);
-    if(nums.length<=1){
-        return false; 
+    if (nums.length <= 1) {
+        return false;
     }
-    else{
-        while(low<nums.length && high<nums.length){
-            if(low!=high && nums[high]-nums[low]>k){
+    else {
+        //Two pointer approach
+        while (low < nums.length && high < nums.length) {
+            if (low != high && nums[high] - nums[low] > k) {
                 low++;
             }
-            else if(nums[high]-nums[low]<k){
+            else if (nums[high] - nums[low] < k) {
                 high++;
             }
-            else{
+            else {
                 return true;
             }
         }
     }
-    return false; 
+    return false;
 }
 
+//Testcases
 console.log('Testcase1');
-console.log(findPairs([5, 20, 3, 2, 5, 80],78));
+console.log(findPairs([5, 20, 3, 2, 5, 80], 78));
 
 console.log('Testcase1');
-console.log(findPairs([90, 70, 20, 80, 50],45));
+console.log(findPairs([90, 70, 20, 80, 50], 45));
+
+//Solution: https://www.tutorialcup.com/interview/array/find-pair-with-given-difference.htm

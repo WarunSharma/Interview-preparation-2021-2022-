7556ad595215fc6c396d9a4648e8fcbaf3dbbbb2
Warun Sharma <warunsharma.95@gmail.com>
2021-07-28 20:15:36 +0530

    Added comments in Find Triplets with sum less than K

--- Data Structure & Algorithms/Searching&Sorting/Find_Triplets_With_Sum_Less_Than_K/FindTripletsWithSumLessThanK_Solution.js
+++ Data Structure & Algorithms/Searching&Sorting/Find_Triplets_With_Sum_Less_Than_K/FindTripletsWithSumLessThanK_Solution.js
@@ -1,29 +1,51 @@
 
-function countTriplets(arr,n,sum){
-    let count=0;
-    arr=arr.sort((a,b)=>a-b);
+/*
+*
+* @param nums Given array
+* @param n size of array
+* @param sum GIven Sum
+* @returns result
+*
+*/
+
+
+function countTriplets(arr, n, sum) {
+    //Initialize the result
+    let result = 0;
+    //Sort an array
+    arr = arr.sort((a, b) => a - b);
     console.log(arr);
-    for(let i=0;i<n-2;i++){
-        let left=i+1,right=n-1;
-        while(left<right){
-            if(arr[i]+arr[left]+arr[right]>=sum){
+    //Fix the first element
+    for (let i = 0; i < n - 2; i++) {
+        //Two pointer approach
+        //left is index of second element
+        //right is index of third element
+        let left = i + 1, right = n - 1;
+        while (left < right) {
+            // If sum of current triplet is more or equal,
+            // move right corner to look for smaller values
+            if (arr[i] + arr[left] + arr[right] >= sum) {
                 right--;
             }
-            else{
-                count+=right-left;
+            // Else move left corner
+            else {
+                // This is important. For current i and j, there
+                // can be total k-j third elements.
+                result += right - left;
                 ++left;
             }
         }
     }
 
-    return count;
+    return result;
 }
 
+//Testcases
 console.log('Testcase1');
-console.log(countTriplets([-2, 0, 1, 3],4,2));
+console.log(countTriplets([-2, 0, 1, 3], 4, 2));
 
 console.log('Testcase2');
-console.log(countTriplets([5, 1, 3, 4, 7],5,12));
+console.log(countTriplets([5, 1, 3, 4, 7], 5, 12));
 
 console.log('Testcase3');
-console.log(countTriplets([30,8,23,6,10,9,31,7,19,20,1,33,21,27,28,3,25,26],18,86));
+console.log(countTriplets([30, 8, 23, 6, 10, 9, 31, 7, 19, 20, 1, 33, 21, 27, 28, 3, 25, 26], 18, 86));

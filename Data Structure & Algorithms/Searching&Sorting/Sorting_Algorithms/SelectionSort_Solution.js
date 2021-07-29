78d64302f7c51f40e14b8c045841e95b5157474d
Warun Sharma <warunsharma.95@gmail.com>
2021-07-29 22:28:52 +0530

    Added comments in Selection Sort solution

--- Data Structure & Algorithms/Searching&Sorting/Sorting_Algorithms/SelectionSort_Solution.js
+++ Data Structure & Algorithms/Searching&Sorting/Sorting_Algorithms/SelectionSort_Solution.js
@@ -34,25 +34,39 @@ arr[] = 64 25 12 22 11
 
 */
 
-function selectionSort(nums){
+/*
+*
+* @param nums Given array
+* @returns nums Sorted array
+*
+*/
+
+//The selection sort algorithm sorts an array by repeatedly finding the minimum element (considering ascending order) from unsorted part and putting it at the beginning.
+function selectionSort(nums) {
     let temp;
-    for(let i=0;i<nums.length-1;++i){
-        let minIdx=i;
-        for(let j=i+1;j<nums.length;++j){
-            if(nums[j]<nums[minIdx]){
-                minIdx=j;
+    // One by one move boundary of unsorted subarray
+    for (let i = 0; i < nums.length - 1; ++i) {
+        let minIdx = i;
+
+        // Find the minimum element in unsorted array
+        for (let j = i + 1; j < nums.length; ++j) {
+            if (nums[j] < nums[minIdx]) {
+                minIdx = j;
             }
         }
-        temp=nums[minIdx];
-        nums[minIdx]=nums[i];
-        nums[i]=temp;
+
+        // Swap the found minimum element with the first element
+        temp = nums[minIdx];
+        nums[minIdx] = nums[i];
+        nums[i] = temp;
     }
 
     return nums;
 }
 
+//Testcases
 console.log('Testcase1');
-console.log(selectionSort([1,4,2,8,9,3,6]));
+console.log(selectionSort([1, 4, 2, 8, 9, 3, 6]));
 
 console.log('Testcase2');
 console.log(selectionSort([1, 2, 3, 4, 6, 8, 9]));

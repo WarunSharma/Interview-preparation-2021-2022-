b4bcc08c5b36923750e9b5f65dc575c2b0e6a490
Warun Sharma <warunsharma.95@gmail.com>
2021-07-29 21:54:56 +0530

    Added comments in Value Equal to index Value solution

--- Data Structure & Algorithms/Searching&Sorting/Value_Equal_To_Index_Value/ValueEqualToIndexValue_Solution.js
+++ Data Structure & Algorithms/Searching&Sorting/Value_Equal_To_Index_Value/ValueEqualToIndexValue_Solution.js
@@ -1,17 +1,31 @@
 
-function valueEqualToIndex(arr,n){
-    let res=[];
-    for(let i=0;i<n;++i){
-        if(arr[i]==i+1){
+/*
+*
+* Time Complexity: O(n)
+* Space Complexity: O(1)
+*
+* @param arr Given array
+* @param n Size of array
+* @returns res resultant array
+*
+*/
+
+/*
+* Linear Search
+*/
+function valueEqualToIndex(arr, n) {
+    let res = [];
+    for (let i = 0; i < n; ++i) {
+        if (arr[i] == i + 1) {
             res.push(arr[i]);
         }
     }
-    res=res.length>0?res:res=["Not Found"]
+    res = res.length > 0 ? res : res = ["Not Found"]
     return res;
 }
 
 console.log("Testcase1");
-console.log(valueEqualToIndex([34,23,12],3));
+console.log(valueEqualToIndex([34, 23, 12], 3));
 
 console.log("Testcase2");
-console.log(valueEqualToIndex([34,23,12,4,56,6],6));
+console.log(valueEqualToIndex([34, 23, 12, 4, 56, 6], 6));

1fa0aac692d6e9dd03ceac32dbd4e73f835d23b1
Warun Sharma <warunsharma.95@gmail.com>
2021-07-29 06:01:28 +0530

    Added comments in Searching in an array where adjacent differ by at most k

--- Data Structure & Algorithms/Searching&Sorting/Searching_In_An_Array_Where_Adjacent_Differ_By_At_Most_K/SearchingInAnArrayWhereAdjacentDifferByAtMostK_Solution.js
+++ Data Structure & Algorithms/Searching&Sorting/Searching_In_An_Array_Where_Adjacent_Differ_By_At_Most_K/SearchingInAnArrayWhereAdjacentDifferByAtMostK_Solution.js
@@ -1,16 +1,33 @@
 
+/*
+*
+* @param nums Given array
+* @param k Step difference
+* @param x Target element
+* @returns index
+*
+*/
+
 function search(nums,k,x){
+    // Traverse the given array starting from
+    // leftmost element
     let i=0;
     while(i<nums.length){
+        // If x is found at index i
         if(nums[i]==x){
             return i;
         }
 
+        // Jump the difference between current
+        // array element and x divided by k
+        // We use max here to make sure that i
+        // moves at-least one step ahead.
         i=i+ Math.max(1,Math.floor(Math.abs(nums[i]-x)/k));
     }
     return -1;
 }
 
+//Testcases
 console.log('Testcase1');
 console.log(search([4, 5, 6, 7, 6],1,6));
 

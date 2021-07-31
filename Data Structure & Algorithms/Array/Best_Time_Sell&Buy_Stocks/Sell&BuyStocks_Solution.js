b5e242a100643ee6a1634bfb69e6a50b5c30b363
Warun Sharma <warunsharma.95@gmail.com>
2021-07-31 15:25:28 +0530

    Added comments in Sell & buy stocks solution

--- Data Structure & Algorithms/Array/Best_Time_Sell&Buy_Stocks/Sell&BuyStocks_Solution.js
+++ Data Structure & Algorithms/Array/Best_Time_Sell&Buy_Stocks/Sell&BuyStocks_Solution.js
@@ -2,25 +2,34 @@
 /*
 * Time Complexity: O(n)
 * Space Complexity: O(1)
+*
+* @param stocks Given stocks prizes in an array
+* @returns maximum profit
 */
 
-let sellAndBuyStocks = function(stocks){
-    let min=stocks[0];
-    let profit=0;
-    for(let i=1;i<stocks.length;++i){
-        if(stocks[i]<min){
-            min=stocks[i];
+let sellAndBuyStocks = function (stocks) {
+    //Initialize min with first stock prize
+    let min = stocks[0];
+
+    //Initialize profit with 0
+    let profit = 0;
+    for (let i = 1; i < stocks.length; ++i) {
+        if (stocks[i] < min) {
+            //Reinitialize min if lesser stock value is found
+            min = stocks[i];
         }
-        else{
-            profit=Math.max(profit,stocks[i]-min);
+        else {
+            //Recalculate profit
+            profit = Math.max(profit, stocks[i] - min);
         }
     }
 
     return profit;
 }
 
+//Testcases
 console.log('Testcase1');
-console.log(sellAndBuyStocks([7,1,5,3,6,4]));
+console.log(sellAndBuyStocks([7, 1, 5, 3, 6, 4]));
 
 console.log('Testcase2');
-console.log(sellAndBuyStocks([7,6,4,3,1]));
+console.log(sellAndBuyStocks([7, 6, 4, 3, 1]));

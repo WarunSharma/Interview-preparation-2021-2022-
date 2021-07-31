b36e268149cb31e0af12644c1e7e66bdedf0792b
Warun Sharma <warunsharma.95@gmail.com>
2021-07-31 15:11:57 +0530

    Added comments in Right array rotation solution

--- Data Structure & Algorithms/Array/Array_Rotation/RightArrayRotation_Solution.js
+++ Data Structure & Algorithms/Array/Array_Rotation/RightArrayRotation_Solution.js
@@ -1,20 +1,29 @@
 
-let rightRotation = function(nums,k){
-    let l=nums.length;
-    k%=l;
-    while(k--){
-        let end=nums[l-1];
-        for(let j=l-2;j>=0;--j){
-            nums[j+1]=nums[j];
+/*
+*
+* @param nums Given array
+* @param k no. of rotations
+* @returns nums rotated array 
+*
+*/
+
+let rightRotation = function (nums, k) {
+    let l = nums.length;
+    k %= l; //rotations 0<=k<l
+    while (k--) {
+        let end = nums[l - 1];
+        for (let j = l - 2; j >= 0; --j) {
+            nums[j + 1] = nums[j];
         }
-        nums[0]=end;
+        nums[0] = end;
     }
 
     return nums;
 }
 
+//Testcases
 console.log('Testcase1')
-console.log(rightRotation([1,2,3,4,5],1))
+console.log(rightRotation([1, 2, 3, 4, 5], 1))
 
 console.log('Testcase2')
-console.log(rightRotation([1,2,3,4,5],3))
+console.log(rightRotation([1, 2, 3, 4, 5], 3))
--- Data Structure & Algorithms/Array/Array_Rotation/RightArrayRotation_Solution_Optimal.js
+++ Data Structure & Algorithms/Array/Array_Rotation/RightArrayRotation_Solution_Optimal.js
@@ -1,37 +1,51 @@
 
-let rightRotation = function(nums,k){
+/*
+*
+* @param nums Given array
+* @param k no. of rotations
+* @returns nums rotated array 
+*
+*/
 
-    let l=nums.length;
-    //If k>l, mod unnecessary rotations
-    k=k%l;
+let rightRotation = function (nums, k) {
+
+    let l = nums.length;
+    //If k>l, mod requires for unnecessary rotations
+    k = k % l; //0<=k<ls
 
     let temp;
 
-    //Reverse first length-k elements
-    for(let i=0,j=l-k-1;i<j;--j,++i){
-        temp=nums[i];
-        nums[i]=nums[j];
-        nums[j]=temp;
+    if (k == 0) {
+        return nums;
     }
+    else {
+        //Reverse first length-k-1 elements
+        for (let i = 0, j = l - k - 1; i < j; --j, ++i) {
+            temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
 
-    //Reverse other half
-    for(let i=l-k,j=l-1;i<j;--j,++i){
-        temp=nums[i];
-        nums[i]=nums[j];
-        nums[j]=temp;
-    }
+        //Reverse other half
+        for (let i = l - k, j = l - 1; i < j; --j, ++i) {
+            temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
 
-    //Reverse whole array
-    for(let i=0,j=l-1;i<j;--j,++i){
-        temp=nums[i];
-        nums[i]=nums[j];
-        nums[j]=temp;
+        //Reverse whole array
+        for (let i = 0, j = l - 1; i < j; --j, ++i) {
+            temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+        return nums;
     }
-    return nums;
 }
 
+//Testcases
 console.log('Testcase1')
-console.log(rightRotation([1,2,3,4,5],1))
+console.log(rightRotation([1, 2, 3, 4, 5], 1))
 
 console.log('Testcase2')
-console.log(rightRotation([1,2,3,4,5],3))
+console.log(rightRotation([1, 2, 3, 4, 5], 4))

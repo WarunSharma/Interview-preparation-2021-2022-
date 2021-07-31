c08d25029a4055424c40dce3b5b95e2e33737d17
Warun Sharma <warunsharma.95@gmail.com>
2021-07-31 15:15:55 +0530

    Added comments in Array Rotation solution

--- Data Structure & Algorithms/Array/Array_Rotation/ArrayRotation_General_Solution_Optimal.js
+++ Data Structure & Algorithms/Array/Array_Rotation/ArrayRotation_General_Solution_Optimal.js
@@ -1,70 +1,79 @@
 
-let leftRotation = function(nums,t,k){
-    let l=nums.length;
-    k%=l;
-    
-    if(t=='R'){
+/*
+*
+* @param nums Given array
+* @param t Rotation type
+* @param k no. of rotations
+* @returns nums rotated array 
+*
+*/
+
+let Rotation = function (nums, t, k) {
+    let l = nums.length;
+    k %= l;
+
+    if (t == 'R' || t=='r') {
         let temp;
 
-        //Reverse first length-k elements
-        for(let i=0,j=l-k-1;i<j;--j,++i){
-            temp=nums[i];
-            nums[i]=nums[j];
-            nums[j]=temp;
+        //Reverse first length-k-1 elements
+        for (let i = 0, j = l - k - 1; i < j; --j, ++i) {
+            temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
         }
 
         //Reverse other half
-        for(let i=l-k,j=l-1;i<j;--j,++i){
-            temp=nums[i];
-            nums[i]=nums[j];
-            nums[j]=temp;
+        for (let i = l - k, j = l - 1; i < j; --j, ++i) {
+            temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
         }
 
         //Reverse whole array
-        for(let i=0,j=l-1;i<j;--j,++i){
-            temp=nums[i];
-            nums[i]=nums[j];
-            nums[j]=temp;
+        for (let i = 0, j = l - 1; i < j; --j, ++i) {
+            temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
         }
         return nums;
     }
-    else{
+    else {
 
         let temp;
 
         //Reverse first K elements
-        for(let i=0,j=k-1;i<j;--j,++i){
-            temp=nums[i];
-            nums[i]=nums[j];
-            nums[j]=temp;
+        for (let i = 0, j = k - 1; i < j; --j, ++i) {
+            temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
         }
 
         //Reverse other half
-        for(let i=k,j=l-1;i<j;--j,++i){
-            temp=nums[i];
-            nums[i]=nums[j];
-            nums[j]=temp;
+        for (let i = k, j = l - 1; i < j; --j, ++i) {
+            temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
         }
 
         //Reverse whole array
-        for(let i=0,j=l-1;i<j;--j,++i){
-            temp=nums[i];
-            nums[i]=nums[j];
-            nums[j]=temp;
+        for (let i = 0, j = l - 1; i < j; --j, ++i) {
+            temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
         }
         return nums;
     }
-    
+
 }
 
 console.log('Testcase1')
-console.log(leftRotation([1,2,3,4,5],'R',1))
+console.log(Rotation([1, 2, 3, 4, 5], 'R', 1))
 
 console.log('Testcase2')
-console.log(leftRotation([1,2,3,4,5],'L',1))
+console.log(Rotation([1, 2, 3, 4, 5], 'L', 1))
 
 console.log('Testcase3')
-console.log(leftRotation([1,2,3,4,5],'L',3))
+console.log(Rotation([1, 2, 3, 4, 5], 'L', 3))
 
 console.log('Testcase3')
-console.log(leftRotation([1,2,3,4,5],'R',3))
+console.log(Rotation([1, 2, 3, 4, 5], 'R', 3))
--- Data Structure & Algorithms/Array/Array_Rotation/LeftArrayRotation_Solution_Optimal.js
+++ Data Structure & Algorithms/Array/Array_Rotation/LeftArrayRotation_Solution_Optimal.js
@@ -1,35 +1,44 @@
 
-let leftRotation = function(nums,k){
-    let l=nums.length;
-    k%=l;
-    
+/*
+*
+* @param nums Given array
+* @param k no. of rotations
+* @returns nums rotated array 
+*
+*/
+
+let leftRotation = function (nums, k) {
+    let l = nums.length;
+    k %= l;
+
     let temp;
 
     //Reverse first K elements
-    for(let i=0,j=k-1;i<j;--j,++i){
-        temp=nums[i];
-        nums[i]=nums[j];
-        nums[j]=temp;
+    for (let i = 0, j = k - 1; i < j; --j, ++i) {
+        temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 
     //Reverse next half
-    for(let i=k,j=l-1;i<j;--j,++i){
-        temp=nums[i];
-        nums[i]=nums[j];
-        nums[j]=temp;
+    for (let i = k, j = l - 1; i < j; --j, ++i) {
+        temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 
     //Reverse whole array
-    for(let i=0,j=l-1;i<j;--j,++i){
-        temp=nums[i];
-        nums[i]=nums[j];
-        nums[j]=temp;
+    for (let i = 0, j = l - 1; i < j; --j, ++i) {
+        temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
     return nums;
 }
 
+//Testcases
 console.log('Testcase1')
-console.log(leftRotation([1,2,3,4,5],1))
+console.log(leftRotation([1, 2, 3, 4, 5], 1))
 
 console.log('Testcase2')
-console.log(leftRotation([1,2,3,4,5],3))
+console.log(leftRotation([1, 2, 3, 4, 5], 3))

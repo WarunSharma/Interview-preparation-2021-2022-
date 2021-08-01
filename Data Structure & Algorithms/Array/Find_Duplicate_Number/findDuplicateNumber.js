75acf218523e202429ce51805f1d45e92c159224
Warun Sharma <warunsharma.95@gmail.com>
2021-08-01 15:20:10 +0530

    Added comments in Find Duplicate Number solution

--- Data Structure & Algorithms/Array/Find_Duplicate_Number/findDuplicateNumber.js
+++ Data Structure & Algorithms/Array/Find_Duplicate_Number/findDuplicateNumber.js
@@ -2,30 +2,33 @@
 /*
 * Time Complexity: O(nlogn)
 * Space Complexity: O(1)
+*
+* @param nums Given Array
+* @returns nums[i] Element
 */
 
-var findDuplicate = function(nums) {
-    
-    for(let i=0;i<nums.length;++i){
-        if(nums[Math.abs(nums[i])-1]<0){
+var findDuplicate = function (nums) {
+
+    for (let i = 0; i < nums.length; ++i) {
+        if (nums[Math.abs(nums[i]) - 1] < 0) {
             return Math.abs(nums[i]);
         }
-        else{
-            nums[Math.abs(nums[i])-1]=-1*nums[Math.abs(nums[i])-1];
+        else {
+            nums[Math.abs(nums[i]) - 1] = -1 * nums[Math.abs(nums[i]) - 1];
         }
     }
 
 };
 
-
+//Testcases
 console.log('Testcase1')
-console.log(findDuplicate([1,1,2]));
+console.log(findDuplicate([1, 1, 2]));
 
 console.log('Testcase2')
-console.log(findDuplicate([1,3,4,2,2]));
+console.log(findDuplicate([1, 3, 4, 2, 2]));
 
 console.log('Testcase1')
-console.log(findDuplicate([3,1,3,4,2]));
+console.log(findDuplicate([3, 1, 3, 4, 2]));
 
 console.log('Testcase1')
-console.log(findDuplicate([1,1]));
+console.log(findDuplicate([1, 1]));

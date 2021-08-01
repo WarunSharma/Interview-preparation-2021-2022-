2dd3be413ff0bc8ff73a0cb52e84d8696c6cd84d
Warun Sharma <warunsharma.95@gmail.com>
2021-08-01 15:23:42 +0530

    Added comments in Intersection of two array solution

--- Data Structure & Algorithms/Array/Intersection_Of_Two_Arrays/IntersectionOfTwoArrays_Solution.js
+++ Data Structure & Algorithms/Array/Intersection_Of_Two_Arrays/IntersectionOfTwoArrays_Solution.js
@@ -2,20 +2,25 @@
 /*
 * Time Complexity: O(n) or O(m)
 * Space Complexity: O(n+m)
+*
+* @param nums1 Given array 1
+* @param nums2 Given array 2
+* @returns res Resultant array
+*
 */
 
-let intersectionOfTwoArrays=function(nums1,nums2){
-    let map=new Array(1001).fill(0);
-    let res=[];
+let intersectionOfTwoArrays = function (nums1, nums2) {
+    let map = new Array(1001).fill(0);
+    let res = [];
 
-    nums1.map(el=>{
-        if(map[el]!=1){
+    nums1.map(el => {
+        if (map[el] != 1) {
             map[el]++;
         }
     })
 
-    nums2.map(el=>{
-        if(map[el]==1){
+    nums2.map(el => {
+        if (map[el] == 1) {
             map[el]--;
             res.push(el);
         }
@@ -24,14 +29,15 @@ let intersectionOfTwoArrays=function(nums1,nums2){
     return res;
 }
 
+//Testcases
 console.log('Testcase1');
-console.log(intersectionOfTwoArrays([1,2,3,5,4],[2,3,1,9]));
+console.log(intersectionOfTwoArrays([1, 2, 3, 5, 4], [2, 3, 1, 9]));
 
 console.log('Testcase2');
-console.log(intersectionOfTwoArrays([1,2,3,5,2,2],[2,3,1,1,1]));
+console.log(intersectionOfTwoArrays([1, 2, 3, 5, 2, 2], [2, 3, 1, 1, 1]));
 
 console.log('Testcase1');
-console.log(intersectionOfTwoArrays([1,],[1,1]));
+console.log(intersectionOfTwoArrays([1,], [1, 1]));
 
 console.log('Testcase1');
-console.log(intersectionOfTwoArrays([],[2,3,1,9]));
+console.log(intersectionOfTwoArrays([], [2, 3, 1, 9]));

9e8559ef819fded0a52ced988db8c2710df26da2
Warun Sharma <warunsharma.95@gmail.com>
2021-08-01 13:36:00 +0530

    Added comments in Equilibrium point solution

--- Data Structure & Algorithms/Array/Equilibrium_Point/EquilibriumPointInAnArray_Solution.js
+++ Data Structure & Algorithms/Array/Equilibrium_Point/EquilibriumPointInAnArray_Solution.js
@@ -1,29 +1,43 @@
 
-let equilibriumPoint = function(nums){
-    let prefixSum=0, sum=0, suffixSum=0;
+/*
+*
+* Time Complexity: O(n)
+* Space Complexity: O(1)
+*
+* @param nums Given array
+* @returns index Equilibrium point
+*
+*/
 
-    sum=nums.reduce((acc,ele)=> {
-       return acc+ele;
+let equilibriumPoint = function (nums) {
+    let prefixSum = 0, sum = 0, suffixSum = 0;
+
+    // Sum of elements in nums
+    sum = nums.reduce((acc, ele) => {
+        return acc + ele;
     });
 
-    for(let i=0;i<nums.length;++i){
-        suffixSum=sum-prefixSum-nums[i];
+    for (let i = 0; i < nums.length; ++i) {
+        //Suffix sum
+        suffixSum = sum - prefixSum - nums[i];
 
-        if(prefixSum==suffixSum){
+        if (prefixSum == suffixSum) {
             return i;
         }
 
-        prefixSum+=nums[i];
+        //Prefix sum
+        prefixSum += nums[i];
     }
-    
+
     return -1;
 }
 
+//Testcases
 console.log('Testcase1');
-console.log(equilibriumPoint([1,3,5,2,2]));
+console.log(equilibriumPoint([1, 3, 5, 2, 2]));
 
 console.log('Testcase2');
 console.log(equilibriumPoint([1]));
 
 console.log('Testcase3');
-console.log(equilibriumPoint([1,2]))
+console.log(equilibriumPoint([1, 2]))

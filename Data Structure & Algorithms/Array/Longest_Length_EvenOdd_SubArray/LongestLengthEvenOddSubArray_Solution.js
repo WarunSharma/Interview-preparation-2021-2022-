aa2edf5097c7593d38d35705db3edf08bc6d3108
Warun Sharma <warunsharma.95@gmail.com>
2021-08-02 18:27:27 +0530

    Added comments in Longest Length Even Odd SubArray

--- Data Structure & Algorithms/Array/Longest_Length_EvenOdd_SubArray/LongestLengthEvenOddSubArray_Solution.js
+++ Data Structure & Algorithms/Array/Longest_Length_EvenOdd_SubArray/LongestLengthEvenOddSubArray_Solution.js
@@ -1,25 +1,31 @@
 
 /*
+*
 * Time Complexity: O(n)
 * Space Complexity: O(1)
+*
+* @param nums Given array
+* @returns res Result
+*
 */
 
-let longestLengthEvenOddSubarray=function(nums){
-    let maxLength=1,curLength=1;
-    for(let i=1;i<nums.length;++i){
-        if((nums[i]%2==0 && nums[i-1]%2==1) || (nums[i]%2==1 && nums[i-1]%2==0)){
+let longestLengthEvenOddSubarray = function (nums) {
+    let maxLength = 1, curLength = 1;
+    for (let i = 1; i < nums.length; ++i) {
+        if ((nums[i] % 2 == 0 && nums[i - 1] % 2 == 1) || (nums[i] % 2 == 1 && nums[i - 1] % 2 == 0)) {
             ++curLength;
         }
-        else{
-            maxLength=Math.max(curLength,maxLength);
-            curLength=1;
+        else {
+            maxLength = Math.max(curLength, maxLength);
+            curLength = 1;
         }
     }
 
-    maxLength=Math.max(curLength,maxLength);
+    maxLength = Math.max(curLength, maxLength);
     return maxLength;
 }
 
+//Testcases
 console.log('Testcase1');
 console.log(longestLengthEvenOddSubarray([1, 2, 3, 4, 5, 7, 9]));
 

22a0cf267df2c01054a0a41f785abab25a4eba50
Warun Sharma <warunsharma.95@gmail.com>
2021-08-02 06:13:20 +0530

    Added comments in Leaders In An Array solution

--- Data Structure & Algorithms/Array/Leaders_In_An_Array/LeadersInAnArray_Solution.js
+++ Data Structure & Algorithms/Array/Leaders_In_An_Array/LeadersInAnArray_Solution.js
@@ -2,23 +2,27 @@
 /*
 * Time Complexity: O(n)
 * Space Complexity: O(1)
+*
+* @param nums Given array
+* @returns res Resultant array
 */
 
-let leardersInArray = function(nums){
-    
-    let max= Number.MIN_SAFE_INTEGER, res=[];
+let leardersInArray = function (nums) {
 
-    for(let i=nums.length-1;i>=0;--i){
-        if(nums[i]>max){
+    let max = Number.MIN_SAFE_INTEGER, res = [];
+
+    for (let i = nums.length - 1; i >= 0; --i) {
+        if (nums[i] > max) {
             res.push(nums[i]);
-            max=nums[i];
+            max = nums[i];
         }
     }
 
-    res=res.reverse();
+    res = res.reverse();
 
     return res;
 }
 
+//Testcases
 console.log('Testcase1');
-console.log(leardersInArray([16,17,4,5,2]));
+console.log(leardersInArray([16, 17, 4, 5, 2]));

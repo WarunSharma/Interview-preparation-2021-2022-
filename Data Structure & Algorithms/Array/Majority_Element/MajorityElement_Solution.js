3cf075dc885c042636c0751e96e89e57fbce170a
Warun Sharma <warunsharma.95@gmail.com>
2021-08-02 18:33:56 +0530

    Added comments in Majority Element solution

--- Data Structure & Algorithms/Array/Majority_Element/MajorityElement_Solution.js
+++ Data Structure & Algorithms/Array/Majority_Element/MajorityElement_Solution.js
@@ -1,12 +1,25 @@
-//Time Complexity: O(nlogn), Space Compexity: O(1)
+/*
+* 
+* Time Complexity: O(nlogn)
+* Space Compexity: O(1)
+*
+* @param nums Given array
+* @returns result
+*
+*/
 
-let majorityElement = function(nums){
-    let sortedNums=nums.sort((a,b)=>a-b);
-    return sortedNums[Math.floor(nums.length/2)];
+//Note: Majority element is present
+let majorityElement = function (nums) {
+    //Sort the array
+    let sortedNums = nums.sort((a, b) => a - b);
+
+    //If array has majority element which occurs more than n/2 times in an array, it must present at middle index
+    return sortedNums[Math.floor(nums.length / 2)];
 }
 
+//Testcases
 console.log('Testcase1');
-console.log(majorityElement([3,2,3]));
+console.log(majorityElement([3, 2, 3]));
 
 console.log('Testcase2');
-console.log(majorityElement([2,2,1,1,1,2,2]));
+console.log(majorityElement([2, 2, 1, 1, 1, 2, 2]));

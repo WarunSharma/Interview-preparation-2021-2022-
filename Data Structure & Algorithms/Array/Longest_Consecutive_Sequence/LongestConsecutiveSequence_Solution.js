6cd1ee69cb51c6cabbb456305b382df3d8aa0777
Warun Sharma <warunsharma.95@gmail.com>
2021-08-02 18:09:42 +0530

    Added comments in Longest Consecutive Sequence solution

--- Data Structure & Algorithms/Array/Longest_Consecutive_Sequence/LongestConsecutiveSequence_Solution.js
+++ Data Structure & Algorithms/Array/Longest_Consecutive_Sequence/LongestConsecutiveSequence_Solution.js
@@ -1,43 +1,55 @@
-// Time Complexity: O(nlogn)
+/*
+*
+* Time Complexity: O(nlogn)
+* Space Complexity:O(1)
+*
+* @param nums Given array
+* @returns res Result
+*
+*/
 
-let longestConsecutiveSequence=function(nums){
-    
+let longestConsecutiveSequence = function (nums) {
+
+    // Empty array or single element array, no meaning of consecutive elements
+    if (nums.length == 0 || nums.length == 1)
+        return nums.length;
     //Sort the array
-    let sortedNums = nums.sort((a,b)=>a-b);
+    let sortedNums = nums.sort((a, b) => a - b);
 
     //Define current sequence count and result
-    let curSeqCount=1,res=1;
-    for(let i=1;i<sortedNums.length;++i){
+    let curSeqCount = 1, res = 1;
+    for (let i = 1; i < sortedNums.length; ++i) {
 
         //If elements are consecutive increment sequence count
-        if(sortedNums[i]==sortedNums[i-1]+1){
+        if (sortedNums[i] == sortedNums[i - 1] + 1) {
             ++curSeqCount;
         }
 
         //If consecutive elements then skip
-        else if(sortedNums[i]==sortedNums[i-1]){
+        else if (sortedNums[i] == sortedNums[i - 1]) {
             continue;
         }
 
         //If elements are not consecutive reinitialize current sequence count
-        else{
-            curSeqCount=1;
+        else {
+            curSeqCount = 1;
         }
 
-        res=Math.max(res,curSeqCount);
+        res = Math.max(res, curSeqCount);
     }
 
     return res;
 }
 
+//Testcases
 console.log('Testcase1')
-console.log(longestConsecutiveSequence([100,4,200,1,3,2]));
+console.log(longestConsecutiveSequence([100, 4, 200, 1, 3, 2]));
 
 console.log('Testcase2')
-console.log(longestConsecutiveSequence([0,3,7,2,5,8,4,6,0,1]));
+console.log(longestConsecutiveSequence([0, 3, 7, 2, 5, 8, 4, 6, 0, 1]));
 
 console.log('Testcase3')
-console.log(longestConsecutiveSequence([1,1,1]));
+console.log(longestConsecutiveSequence([1, 1, 1]));
 
 console.log('Testcase4')
-console.log(longestConsecutiveSequence([1,1,1,2,2]));
+console.log(longestConsecutiveSequence([1, 1, 1, 2, 2]));

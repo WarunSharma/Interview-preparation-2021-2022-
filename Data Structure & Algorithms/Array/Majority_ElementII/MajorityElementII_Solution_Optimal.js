59db08a109c88f728970084cca1fd4ca8d569f94
Warun Sharma <warunsharma.95@gmail.com>
2021-08-03 05:38:16 +0530

    Added comments in optimal solution of Majority Element II

--- Data Structure & Algorithms/Array/Majority_ElementII/MajorityElementII_Solution_Optimal.js
+++ Data Structure & Algorithms/Array/Majority_ElementII/MajorityElementII_Solution_Optimal.js
@@ -1,22 +1,34 @@
 
+/*
+*
+* Time Complexity: O(n)
+* Space Complexity: O(n)
+*
+* @param nums Given array
+* @returns res Resultant array
+*
+*/
 
-let majorityElementII = function(nums){
-    let res=[];
-    let map=new Map();
+//There can only be 2 elements maximum which can occur more than n/3 times in the array
 
+let majorityElementII = function (nums) {
+    let res = [];
+    let map = new Map();
+
+    //Storing no. of occurrences of elements in a map
     nums.forEach(element => {
-        if(map.has(element)){
-            let temp=map.get(element);
-            map.set(element,temp+1);
+        if (map.has(element)) {
+            let temp = map.get(element);
+            map.set(element, temp + 1);
         }
-        else{
-            map.set(element,1);
+        else {
+            map.set(element, 1);
         }
     });
 
-    for(let i of map){
+    for (let i of map) {
         console.log(i)
-        if(i[1]>Math.floor(nums.length/3)){
+        if (i[1] > Math.floor(nums.length / 3)) {
             res.push(i[0]);
         }
     }
@@ -24,12 +36,12 @@ let majorityElementII = function(nums){
     return res;
 }
 
-
+//Testcases
 console.log('Testcase1');
-console.log(majorityElementII([3,2,3]));
+console.log(majorityElementII([3, 2, 3]));
 
 console.log('Testcase1');
 console.log(majorityElementII([1]));
 
 console.log('Testcase1');
-console.log(majorityElementII([1,2]));
+console.log(majorityElementII([1, 2]));

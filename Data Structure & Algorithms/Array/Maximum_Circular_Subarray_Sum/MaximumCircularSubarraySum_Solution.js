714cb071b4d8502299c9b9d063f3e9541cd1443b
Warun Sharma <warunsharma.95@gmail.com>
2021-08-03 18:19:51 +0530

    Added comments in Maximum Circular Subarray sum

--- Data Structure & Algorithms/Array/Maximum_Circular_Subarray_Sum/MaximumCircularSubarraySum_Solution.js
+++ Data Structure & Algorithms/Array/Maximum_Circular_Subarray_Sum/MaximumCircularSubarraySum_Solution.js
@@ -1,12 +1,17 @@
 
 /*
+*
 * Time Complexity: O(n)
 * Space Complexity: O(1)
+*
+* @param nums Given array
+* @returns maxSum Result
+*
 */
 
 let kedaneMaxSubarraySum = function(nums){
     
-    let maxSum=0,curSum=0;
+    let maxSum=Number.MIN_SAFE_INTEGER,curSum=0;
     for(let i=0;i<nums.length;++i){
         curSum+=nums[i];
         if(curSum<0){
@@ -20,14 +25,22 @@ let kedaneMaxSubarraySum = function(nums){
 
 let maxCircularSubarraySum = function(nums){
     
+    // Case 1: get the maximum sum using standard kadane'
+    // s algorithm
     let maxSum=kedaneMaxSubarraySum(nums);
-    let negNums=nums.map(el=>-1*el);
+
+    // Case 2: Now find the maximum sum that includes
+    // corner elements.
+    let negNums=nums.map(el=>-1*el); // invert the array (change sign)
     let minSum=-1*kedaneMaxSubarraySum(negNums);
-    let totalSum=nums.reduce((acc,el)=>acc+el)
+    let totalSum=nums.reduce((acc,el)=>acc+el);
+
+    // The maximum circular sum will be maximum of two sums
     return Math.max(maxSum,(totalSum-minSum));
 
 }
 
+//Testcases
 console.log('Testcase1');
 console.log(maxCircularSubarraySum([10,-3,-4,7,6,5,-4,-1]));
 

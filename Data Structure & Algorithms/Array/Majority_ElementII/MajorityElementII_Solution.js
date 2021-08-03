f688f19180a3bf8db796907f1458861251be64b7
Warun Sharma <warunsharma.95@gmail.com>
2021-08-03 05:33:33 +0530

    Added comments in solution of Majority Element II

--- Data Structure & Algorithms/Array/Majority_ElementII/MajorityElementII_Solution.js
+++ Data Structure & Algorithms/Array/Majority_ElementII/MajorityElementII_Solution.js
@@ -1,36 +1,47 @@
 
-let majorityElementII = function(nums){
-    let sortedNums= nums.sort((a,b)=>a-b);
+/*
+*
+* Time Complexity: O(n2)
+* Space Complexity: O(1)
+*
+* @param nums Given array
+* @returns res Resultant array
+*
+*/
 
-    let count=1;
-    let res=[];
-    for(let i=0;i<sortedNums.length-1;++i){
-        for(let j=i+1;j<sortedNums.length;++j){
-            if(sortedNums[i]==sortedNums[j]){
+//There can only be 2 elements maximum which can occur more than n/3 times in the array
+let majorityElementII = function (nums) {
+    let sortedNums = nums.sort((a, b) => a - b);
+
+    let count = 1;
+    let res = [];
+    for (let i = 0; i < sortedNums.length - 1; ++i) {
+        for (let j = i + 1; j < sortedNums.length; ++j) {
+            if (sortedNums[i] == sortedNums[j]) {
                 count++;
             }
-            else{
-                if(count>Math.floor(sortedNums.length/3)){
+            else {
+                if (count > Math.floor(sortedNums.length / 3)) {
                     res.push(sortedNums[i]);
                 }
-                count=1;
+                count = 1;
                 break;
             }
         }
     }
-    if(count>Math.floor(sortedNums.length/3)){
-        res.push(sortedNums[nums.length-1]);
+    if (count > Math.floor(sortedNums.length / 3)) {
+        res.push(sortedNums[nums.length - 1]);
     }
 
     return res;
 }
 
-
+//Testcases
 console.log('Testcase1');
-console.log(majorityElementII([3,2,3]));
+console.log(majorityElementII([3, 2, 3]));
 
 console.log('Testcase1');
 console.log(majorityElementII([1]));
 
 console.log('Testcase1');
-console.log(majorityElementII([1,2]));
+console.log(majorityElementII([1, 2]));

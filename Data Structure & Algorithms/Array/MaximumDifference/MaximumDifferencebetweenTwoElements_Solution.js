1295113c3af499ea2fe4a05d0c1619f1adace985
Warun Sharma <warunsharma.95@gmail.com>
2021-08-03 20:29:32 +0530

    Modified solution for Maximum Difference Between two elements and added comments

--- Data Structure & Algorithms/Array/MaximumDifference/MaximumDifferencebetweenTwoElements_Solution.js
+++ Data Structure & Algorithms/Array/MaximumDifference/MaximumDifferencebetweenTwoElements_Solution.js
@@ -1,20 +1,39 @@
 
-let maximumDifference = function(nums){
-    let maxDiff=nums[1]-nums[0];
-    let min=nums[0];
+/*
+*
+* Time Complexity: O(n)
+* Space Complexity:O(1)
+*
+* @param nums Given array
+* @returns maxDiff Maximum Difference
+*
+*/
 
-    for(let i=2;i<nums.length;++i){
-        if(nums[i]>min){
-            maxDiff=Math.max(nums[i]-min,maxDiff);
-        }
-        else{
+
+let maximumDifference = function(nums){
+
+    //Initialize
+    let currDiff=0,maxDiff=Number.MIN_SAFE_INTEGER;
+    let min=Number.MAX_SAFE_INTEGER;
+
+    for(let i=0;i<nums.length;++i){
+        
+        //Replace min with new minimum value in the nums
+        if(min>nums[i]){
             min=nums[i];
         }
+
+        //Recalculate CurrDiff 
+        currDiff=nums[i]-min;
+
+        //Replace maxDiff with new current Difference and previous maximum Difference
+        maxDiff=Math.max(currDiff,maxDiff);
     }
 
     return maxDiff;
 }
 
+//Testcases
 console.log('Testcase1');
 console.log(maximumDifference([2, 3, 10, 6, 4, 8, 1]));
 

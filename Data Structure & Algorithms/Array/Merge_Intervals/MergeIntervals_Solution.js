7447fb6e21b9da0920f7b65badb2d0f53054ff41
Warun Sharma <warunsharma.95@gmail.com>
2021-08-04 05:58:12 +0530

    Added comments in Merge Intervals solution

--- Data Structure & Algorithms/Array/Merge_Intervals/MergeIntervals_Solution.js
+++ Data Structure & Algorithms/Array/Merge_Intervals/MergeIntervals_Solution.js
@@ -1,24 +1,42 @@
 
-let mergeIntervals=function(intervals){
-    intervals.sort((a,b)=>a[0]-b[0]);
-    let res=[];
-    res[0]=intervals[0];
-    for(let i=1;i<intervals.length;++i){
-        if(intervals[i][0]>res[res.length-1][1]){
+/*
+*
+* Time Complexity: O(n)
+* Space Complexity: O(1)
+*
+* @param intervals Given array
+* @returns res Resultant array
+*
+*/
+
+let mergeIntervals = function (intervals) {
+
+    //Sort the array
+    intervals.sort((a, b) => a[0] - b[0]);
+    let res = [];
+
+    //Insert first element
+    res.push(intervals[0]);
+    for (let i = 1; i < intervals.length; ++i) {
+
+        //Top element in res is not ovverlapping with (i)th element in intervals
+        if (intervals[i][0] > res[res.length - 1][1]) {
             res.push(intervals[i]);
         }
-        else{
-            res[res.length-1][1]=Math.max(res[res.length-1][1],intervals[i][1]);
+        else {
+            //In case  of Overlapping interval modify end element of res top element 
+            res[res.length - 1][1] = Math.max(res[res.length - 1][1], intervals[i][1]);
         }
     }
     return res;
 }
 
+//Testcases
 console.log('Testcase1');
-console.log(mergeIntervals([[8,10],[1,6],[15,18]]));
+console.log(mergeIntervals([[8, 10], [1, 6], [15, 18]]));
 
 console.log('Testcase2');
-console.log(mergeIntervals([[1,4],[4,5]]));
+console.log(mergeIntervals([[1, 4], [4, 5]]));
 
 console.log('Testcase3');
-console.log(mergeIntervals([[1,3],[2,6],[8,10],[15,18]]));
+console.log(mergeIntervals([[1, 3], [2, 6], [8, 10], [15, 18]]));

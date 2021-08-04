7131c7d5e527084e2b62ae05ad3fd20c22464314
Warun Sharma <warunsharma.95@gmail.com>
2021-08-04 05:41:26 +0530

    Added commments in Maximum K Sum subarray solution

--- Data Structure & Algorithms/Array/MaximumKSumSubarray/MaximumKSumSubarray_Solution.js
+++ Data Structure & Algorithms/Array/MaximumKSumSubarray/MaximumKSumSubarray_Solution.js
@@ -1,32 +1,40 @@
 
 /*
+*
 * Time Complexity: O(n)
 * Space Complexity: O(1)
 *
+* @param nums Given array
+* @param k Given size
+* @returns sum Resultant sum
+*
 */
-
 
 let maximumKSumSubarray = function(nums,k){
 
+    //Returns if array length is less than subaaray size
     if(k>nums.length){
         return 'Invalid'
     }
     let sum=0;
+
+    //Sum of first K elements
     for(let i=0;i<k;++i){
         sum+=nums[i];
     }
 
     let curSum=sum;
+
+    //Removing (i-k)th element from curSum and add current elements 
     for(let i=k;i<nums.length;++i){
         curSum=curSum-nums[i-k]+nums[i];
-        if(curSum>sum){
-            sum=curSum;
-        }
+        sum=Math.max(sum,curSum);
     }
 
     return sum;
 }
 
+//Testcases
 console.log('Testcase1');
 console.log(maximumKSumSubarray([100, 200, 300, 400],2));
 

ec8a0fc1332e9ed066a72e739a436303b55dbece
Warun Sharma <warunsharma.95@gmail.com>
2021-08-05 18:29:22 +0530

    Added comments in Minimum number of Jumps solution

--- Data Structure & Algorithms/Array/Minimum_Number_Of_Jumps/MinimumNumberOfjumps_Solution.js
+++ Data Structure & Algorithms/Array/Minimum_Number_Of_Jumps/MinimumNumberOfjumps_Solution.js
@@ -1,39 +1,48 @@
 
 /*
+*
 * Time Complexity: O(n2)
 * Space Complexity: O(n)
+*
+* @param nums Given array
+* @returns Minimum Jumps
+*
 */
 
-let minimumNumber=function(nums){
-    let length=nums.length;
+let minimumNumber = function (nums) {
+    let length = nums.length;
 
-    let dp=new Array(nums.length).fill(Number.MAX_SAFE_INTEGER);
-    dp[0]=0;
+    //dp array for memoization
+    let dp = new Array(nums.length).fill(Number.MAX_SAFE_INTEGER);
+    dp[0] = 0;
 
-    if(nums[0]==0 && nums.length>1){
+    //Can't move forward as first element is 0
+    if (nums[0] == 0 && nums.length > 1) {
         return -1;
     }
     else {
-        for(let i=1;i<length;++i){
-            for(let j=0;j<i;++j){
-                if(dp[j]!=Number.MAX_SAFE_INTEGER && nums[j]+j>=i){
-                    if(dp[j]+1<dp[i]){
-                        dp[i]=dp[j]+1;
+        for (let i = 1; i < length; ++i) {
+            for (let j = 0; j < i; ++j) {
+                //Update dp array if lesser steps to reach at (i)th place
+                if (dp[j] != Number.MAX_SAFE_INTEGER && nums[j] + j >= i) {
+                    if (dp[j] + 1 < dp[i]) {
+                        dp[i] = dp[j] + 1;
                     }
                 }
             }
         }
     }
-    
-    if(dp[length-1]!=Number.MAX_SAFE_INTEGER){
-        return dp[length-1];
+
+    if (dp[length - 1] != Number.MAX_SAFE_INTEGER) {
+        return dp[length - 1];
     }
     return -1;
 
 }
 
+//Testcases
 console.log('Testcase1');
-console.log(minimumNumber([1,2,3,4,5,6]));
+console.log(minimumNumber([1, 2, 3, 4, 5, 6]));
 
 console.log('Testcase2');
 console.log(minimumNumber([1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9]));

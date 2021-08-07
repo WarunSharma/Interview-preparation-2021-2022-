2a029a1d715954d995bef8f2e1c7a6f88589fa8b
Warun Sharma <warunsharma.95@gmail.com>
2021-08-07 17:45:55 +0530

    Added comments in Union of Two arrays

--- Data Structure & Algorithms/Array/Union_Of_Two_Arrays/UnionOfTwoArrays_Solution.js
+++ Data Structure & Algorithms/Array/Union_Of_Two_Arrays/UnionOfTwoArrays_Solution.js
@@ -1,10 +1,18 @@
 
 /*
+*
 * Time Complexity: O(n) or O(m)
 * Space Complexity: O(n+m)
+*
+* @param nums1 Given array 1
+* @param nums2 Given array 2
+* @returns res Resultant array
+*
 */
 
 let unionOfTwoArray=function(nums1,nums2){
+
+    //Set to store all elements
     let set=new Set();
 
     nums1.map(el=>{
@@ -15,6 +23,7 @@ let unionOfTwoArray=function(nums1,nums2){
         set.add(el);
     });
 
+    //Initialize res array
     let res=[];
 
     for(let i of set){
@@ -24,5 +33,6 @@ let unionOfTwoArray=function(nums1,nums2){
     return res.length;
 }
 
+//Testcases
 console.log('Testcase1');
 console.log(unionOfTwoArray([1, 2, 3, 4, 5],[1, 2, 3]));

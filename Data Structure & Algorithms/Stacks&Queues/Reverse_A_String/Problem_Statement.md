526e561d8ad629a9ba059ca8485f9e3ba6aa03fe
Warun Sharma <warunsharma.95@gmail.com>
2021-09-09 18:20:48 +0530

    Added solution for Reverse A String using Stack problem

---
+++ Data Structure & Algorithms/Stacks&Queues/Reverse_A_String/Problem_Statement.md
@@ -0,0 +1,28 @@
+
+https://practice.geeksforgeeks.org/problems/reverse-a-string-using-stack/1
+
+Reverse a string using Stack 
+Easy Accuracy: 54.33% Submissions: 41083 Points: 2
+You are given a string S, the task is to reverse the string using stack.
+
+ 
+
+Example 1:
+
+
+Input: S="GeeksforGeeks"
+Output: skeeGrofskeeG
+ 
+
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function reverse() which takes the string S as an input parameter and returns the reversed string.
+
+ 
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(N)
+
+ 
+
+Constraints:
+1 ≤ length of the string ≤ 100
---
+++ Data Structure & Algorithms/Stacks&Queues/Reverse_A_String/ReverseAString_Solution.js
@@ -0,0 +1,50 @@
+
+class Stack{
+    stackArray=[];
+    top;
+
+    constructor(){
+        this.top=-1;
+    }
+    push(ele){
+        this.stackArray.push(ele);
+        ++this.top;
+    }
+
+    pop(){
+        let ele = this.stackArray.pop();
+        --this.top;
+        return ele;
+    }
+
+    peek(){
+        return this.stackArray[this.top];
+    }
+
+    isEmpty(){
+        return this.top==-1;
+    }
+
+}
+
+function reverseString(str){
+    let stack=new Stack();
+    let temp="";
+    
+    for(let i=0;i<str.length;++i){
+        stack.push(str[i]);
+    }
+
+    for(let i=str.length-1;i>=0;--i){
+        temp+=stack.pop();
+    }
+
+    str=temp;
+
+    return str;
+
+}
+
+
+console.log('Testcase 1');
+console.log(reverseString("GeeksforGeeks"));

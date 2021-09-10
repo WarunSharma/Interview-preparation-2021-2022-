dec223edd061b154c5923c93e2efc27dff0efd45
Warun Sharma <warunsharma.95@gmail.com>
2021-09-10 19:05:51 +0530

    Added solution for Special Stack problem

---
+++ Data Structure & Algorithms/Stacks&Queues/Special_Stack/SpecialStack_Solution.js
@@ -0,0 +1,54 @@
+/**
+ * initialize your data structure here.
+ */
+ var MinStack = function() {
+    this.stack = [];
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MinStack.prototype.push = function(x) {
+    this.stack.unshift(x);
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function() {
+    this.stack.shift();
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function() {
+    return this.stack[0]
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function() {
+    return Math.min(...this.stack)
+};
+
+/** 
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(x)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
+
+let stack=new MinStack();
+stack.push(10);
+stack.push(7);
+stack.push(8);
+console.log(stack.getMin());
+
+
+// Credits: https://leetcode.com/problems/min-stack/discuss/424712/Javascript
+

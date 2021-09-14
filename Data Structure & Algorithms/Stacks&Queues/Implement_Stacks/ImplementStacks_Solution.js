187c7cfd585ad3a3b90e48c5de2eb0f69b90e061
Warun Sharma <warunsharma.95@gmail.com>
2021-09-14 19:34:19 +0530

    Added comments in Implement a Stack solution

--- Data Structure & Algorithms/Stacks&Queues/Implement_Stacks/ImplementStacks_Solution.js
+++ Data Structure & Algorithms/Stacks&Queues/Implement_Stacks/ImplementStacks_Solution.js
@@ -1,40 +1,57 @@
 
-class Stack{
-    stackArray=[];
+/**
+ * Stack Implementation using
+ * an array
+ */
+
+class Stack {
+
+    //Initialize an Array for storage of a stack
+    stackArray = [];
     top;
 
-    constructor(){
-        this.top=-1;
+    //Initialize top
+    constructor() {
+        this.top = -1;
     }
-    push(ele){
+
+    //Push an element in O(1)
+    push(ele) {
         this.stackArray.push(ele);
         ++this.top;
     }
 
-    pop(){
+    //Pop an element in O(1)
+    pop() {
         let ele = this.stackArray.pop();
         --this.top;
         return ele;
     }
 
-    peek(){
+    //Get peek element in O(1)
+    peek() {
         return this.stackArray[this.top];
     }
 
-    isEmpty(){
-        return this.top==-1;
+    isEmpty() {
+        return this.top == -1;
     }
 
 }
 
 
-let stack1=new Stack();
+let stack1 = new Stack();
+
 stack1.push(1);
 stack1.push(2);
-while(!stack1.isEmpty()){
-    console.log(stack1.pop(),' ');
+
+while (!stack1.isEmpty()) {
+    console.log(stack1.pop());
 }
+
+
 stack1.push(3);
-while(!stack1.isEmpty()){
-    console.log(stack1.pop(),' ');
+
+while (!stack1.isEmpty()) {
+    console.log(stack1.pop());
 }

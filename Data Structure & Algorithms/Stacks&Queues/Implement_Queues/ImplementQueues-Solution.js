a98e44c00f3e8956b51098bc86a48cbdb21bc92b
Warun Sharma <warunsharma.95@gmail.com>
2021-09-15 20:42:37 +0530

    Added comments in Implement a Queue solution

--- Data Structure & Algorithms/Stacks&Queues/Implement_Queues/ImplementQueues-Solution.js
+++ Data Structure & Algorithms/Stacks&Queues/Implement_Queues/ImplementQueues-Solution.js
@@ -1,39 +1,49 @@
 
-class Queue{
+/**
+* Queue Implementation using 
+* an array
+*/
+
+class Queue {
     front;
     rear;
-    queueArray=[];
 
-    constructor(){
-        this.front=-1;
-        this.rear=-1;
-    }
-    
-    isEmpty(){
-        return this.front==-1;
+    //Initialize an Array for storage of a queue
+    queueArray = [];
+
+    //Initialize rear and front of the queue
+    constructor() {
+        this.front = -1;
+        this.rear = -1;
     }
 
-    enQueue(ele){
-        if(this.isEmpty()){
+    isEmpty() {
+        return this.front == -1;
+    }
+
+    //Enqueue an element in O(1)
+    enQueue(ele) {
+        if (this.isEmpty()) {
             this.queueArray.push(ele);
             ++this.front;
             ++this.rear;
         }
-        else{
+        else {
             this.queueArray.push(ele);
             ++this.rear;
         }
     }
 
-    deQueue(){
-        if(this.isEmpty()){
+    //Dequeue an element in O(1)
+    deQueue() {
+        if (this.isEmpty()) {
             return -1;
         }
-        else{
-            let ele=this.queueArray.shift();
+        else {
+            let ele = this.queueArray.shift();
             --this.rear;
-            if(this.rear==-1){
-                this.front=-1;
+            if (this.rear == -1) {
+                this.front = -1;
             }
             return ele;
         }
@@ -41,21 +51,22 @@ class Queue{
 
 }
 
-let queue1=new Queue();
+let queue1 = new Queue();
 queue1.enQueue(1);
 queue1.enQueue(2);
 queue1.enQueue(3);
 queue1.enQueue(4);
 
-while(!queue1.isEmpty()){
-    console.log(queue1.deQueue(),' ');
+while (!queue1.isEmpty()) {
+    console.log(queue1.deQueue());
 }
 
+
 queue1.enQueue(4);
 queue1.enQueue(3);
 queue1.enQueue(2);
 queue1.enQueue(1);
 
-while(!queue1.isEmpty()){
-    console.log(queue1.deQueue(),' ');
+while (!queue1.isEmpty()) {
+    console.log(queue1.deQueue());
 }

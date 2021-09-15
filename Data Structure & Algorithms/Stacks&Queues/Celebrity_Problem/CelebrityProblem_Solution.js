928be309afcaea0793b8cfb1de75a96df07683bd
Warun Sharma <warunsharma.95@gmail.com>
2021-09-15 21:11:34 +0530

    Added comments in Celebrity Problem solution

--- Data Structure & Algorithms/Stacks&Queues/Celebrity_Problem/CelebrityProblem_Solution.js
+++ Data Structure & Algorithms/Stacks&Queues/Celebrity_Problem/CelebrityProblem_Solution.js
@@ -1,57 +1,63 @@
 
-class Stack{
-    stackArray=[];
+class Stack {
+    stackArray = [];
     top;
 
-    constructor(){
-        this.top=-1;
+    constructor() {
+        this.top = -1;
     }
-    push(ele){
+    push(ele) {
         this.stackArray.push(ele);
         ++this.top;
     }
 
-    pop(){
+    pop() {
         let ele = this.stackArray.pop();
         --this.top;
         return ele;
     }
 
-    peek(){
+    peek() {
         return this.stackArray[this.top];
     }
 
-    isEmpty(){
-        return this.top==-1;
+    isEmpty() {
+        return this.top == -1;
     }
 
 }
 
-function celebrityCheck(arr){
-    let stack=new Stack();
+function celebrityCheck(arr) {
+    let stack = new Stack();
 
-    for(let i=0;i<arr.length;++i){
+    //Insert array indexes in stack
+    for (let i = 0; i < arr.length; ++i) {
         stack.push(i);
     }
 
-    while(stack.top>=1){
-        let i=stack.pop();
-        let j=stack.pop();
+    while (stack.top >= 1) {
 
-        if(arr[i][j]){
+        //Pop top two elements for comparision
+        let i = stack.pop();
+        let j = stack.pop();
+
+        //If ith person knows jth person
+        if (arr[i][j]) {
             stack.push(j);
         }
-        else{
+        else {
             stack.push(i);
         }
 
     }
 
-    let potentialCelebrity=stack.pop();
+    //Top element of stack is the potential celebrity
+    let potentialCelebrity = stack.pop();
 
-    for(let i=0;i<arr.length;++i){
-        if(i!=potentialCelebrity){
-            if(arr[i][potentialCelebrity]==0 || arr[potentialCelebrity][i]==1){
+    for (let i = 0; i < arr.length; ++i) {
+        if (i != potentialCelebrity) {
+            // If ith element doesn't know potentialCelebrity or potentialCelebrity knows ith element means there is no celebrity.
+            if (arr[i][potentialCelebrity] == 0 || arr[potentialCelebrity][i] == 1) {
                 return -1;
             }
         }
@@ -62,10 +68,10 @@ function celebrityCheck(arr){
 
 
 console.log('Testcase 1');
-console.log(celebrityCheck([[0,1,0],
-    [0,0,0], 
-    [0,1,0]]));
+console.log(celebrityCheck([[0, 1, 0],
+[0, 0, 0],
+[0, 1, 0]]));
 
 console.log('Testcase 2');
-console.log(celebrityCheck([[0,1],
-    [1,0]]));    
+console.log(celebrityCheck([[0, 1],
+[1, 0]]));

2afeb81274fc5c6a490b608c10ef55ed7bcd2fb5
Warun Sharma <warunsharma.95@gmail.com>
2021-09-16 20:32:28 +0530

    Added comments in Implement Two Stacks Using an Array solution

--- Data Structure & Algorithms/Stacks&Queues/Implement_Two_Stacks_Using_An_Array/ImplementTwoStacksUsingAnArray_Solution.js
+++ Data Structure & Algorithms/Stacks&Queues/Implement_Two_Stacks_Using_An_Array/ImplementTwoStacksUsingAnArray_Solution.js
@@ -1,65 +1,75 @@
 
-class TwoStack{
+/**
+ * Two Stacks 
+ * in an array
+ */
+
+class TwoStack {
     top1;
     top2;
     size;
 
-    constructor(size){
-        this.top1=-1;
-        this.top2=size;
-        this.size=size;
-        this.stackArray=new Array(size);
+    //Initialize top of stack1, stack2 and array
+    constructor(size) {
+        this.top1 = -1;
+        this.top2 = size;
+        this.size = size;
+        this.stackArray = new Array(size);
     }
 
-    isEmpty1(){
-        return this.top1==-1;
+    isEmpty1() {
+        return this.top1 == -1;
     }
 
-    isEmpty2(){
-        return this.top2==this.size;
+    isEmpty2() {
+        return this.top2 == this.size;
     }
 
-    push1(ele){
-        this.stackArray[++this.top1]=ele;
+    //Method to push an element ele in stack1
+    push1(ele) {
+        this.stackArray[++this.top1] = ele;
     }
 
-    push2(ele){
-        this.stackArray[--this.top2]=ele;
+    //Method to push an element ele in stack1
+    push2(ele) {
+        this.stackArray[--this.top2] = ele;
     }
 
-    pop1(){
-        if(!this.isEmpty1()){
-            let temp=this.stackArray[this.top1];
-            this.stackArray.splice(this.top1,1);
+    //Method to pop an element from stack1
+    pop1() {
+        if (!this.isEmpty1()) {
+            let temp = this.stackArray[this.top1];
+            this.stackArray.splice(this.top1, 1);
             --this.top1;
             return temp;
         }
-        else{
+        else {
             return -1;
         }
     }
 
-    pop2(){
-        if(!this.isEmpty2()){
-            let temp=this.stackArray[this.top2];
-            this.stackArray.splice(this.top2,1);
+    //Method to pop an element from stack2
+    pop2() {
+        if (!this.isEmpty2()) {
+            let temp = this.stackArray[this.top2];
+            this.stackArray.splice(this.top2, 1);
             ++this.top2;
             return temp;
         }
-        else{
+        else {
             return -1;
         }
-        
+
     }
 
-    show(){
+    show() {
         console.log(this.stackArray);
     }
 
 
 }
 
-let stack1=new TwoStack(10);
+let stack1 = new TwoStack(10);
 stack1.push1(1);
 stack1.push1(2);
 console.log(stack1.pop1());

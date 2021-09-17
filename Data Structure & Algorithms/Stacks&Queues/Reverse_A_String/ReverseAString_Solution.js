078d98cda788978cfabb560bb190f2f2ab3d1f95
Warun Sharma <warunsharma.95@gmail.com>
2021-09-17 17:38:33 +0530

    Formatted document for Reverse a String using a Stack

--- Data Structure & Algorithms/Stacks&Queues/Reverse_A_String/ReverseAString_Solution.js
+++ Data Structure & Algorithms/Stacks&Queues/Reverse_A_String/ReverseAString_Solution.js
@@ -1,45 +1,45 @@
 
-class Stack{
-    stackArray=[];
+class Stack {
+    stackArray = [];
     top;
 
-    constructor(){
-        this.top=-1;
+    constructor() {
+        this.top = -1;
     }
-    push(ele){
+    push(ele) {
         this.stackArray.push(ele);
         ++this.top;
     }
 
-    pop(){
+    pop() {
         let ele = this.stackArray.pop();
         --this.top;
         return ele;
     }
 
-    peek(){
+    peek() {
         return this.stackArray[this.top];
     }
 
-    isEmpty(){
-        return this.top==-1;
+    isEmpty() {
+        return this.top == -1;
     }
 
 }
 
-function reverseString(str){
-    let stack=new Stack();
-    let temp="";
-    
-    for(let i=0;i<str.length;++i){
+function reverseString(str) {
+    let stack = new Stack();
+    let temp = "";
+
+    for (let i = 0; i < str.length; ++i) {
         stack.push(str[i]);
     }
 
-    for(let i=str.length-1;i>=0;--i){
-        temp+=stack.pop();
+    for (let i = str.length - 1; i >= 0; --i) {
+        temp += stack.pop();
     }
 
-    str=temp;
+    str = temp;
 
     return str;
 

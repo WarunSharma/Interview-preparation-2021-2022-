3485f701bed46daf07e86c1d47f8e68fb0861eb7
Warun Sharma <warunsharma.95@gmail.com>
2021-09-17 17:50:31 +0530

    Added solution for Is a Number Prime

---
+++ Data Structure & Algorithms/Basics/Is_A_Number_Prime/IsANumberPrime_Solution.js
@@ -0,0 +1,19 @@
+
+function isNumberPrime(nums){
+    for(let num of nums){
+        let isPrime=true;
+        for(let i=2;i*i<=num;++i){
+            if(num%i==0){
+                isPrime=false;
+            }
+        }
+
+        if(isPrime){
+            console.log('prime');
+        }
+        else{
+            console.log('not prime');
+        }
+
+    }
+}

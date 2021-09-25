471736e1a282ec412277a4c6608ed257689e8e64
Warun Sharma <warunsharma.95@gmail.com>
2021-09-25 13:39:40 +0530

    Added solution for pattern 3 problem in java

--- DSA_Level-1/Basics Of Programming/Patterns/Pattern 3/Pattern3_Solution.java
+++ DSA_Level-1/Basics Of Programming/Patterns/Pattern 3/Pattern3_Solution.java
@@ -4,13 +4,18 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
+
         int n=scn.nextInt();
-        for(int row=n;row>0;--row){
-            for(int col=row;col>0;--col){
+        
+        for(int i=1;i<=n;++i){
+            for(int j=n-i;j>=1;--j){
+                System.out.print("	");
+            }
+            for(int j=1;j<=i;++j){
                 System.out.print("*	");
             }
             System.out.println();
         }
-
+        
     }
 }

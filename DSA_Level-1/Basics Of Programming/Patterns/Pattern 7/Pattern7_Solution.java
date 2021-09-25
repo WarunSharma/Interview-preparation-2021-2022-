fba4fd2cd7b5168d179015b1a8f1139aa9b652e1
Warun Sharma <warunsharma.95@gmail.com>
2021-09-25 13:45:37 +0530

    Added solution for pattern 7 problem in java

---
+++ DSA_Level-1/Basics Of Programming/Patterns/Pattern 7/Pattern7_Solution.java
@@ -0,0 +1,19 @@
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int num=scn.nextInt();
+        for(int i=1;i<=num;++i){
+            for(int j=1;j<=i-1;++j){
+                System.out.print("	");
+            }
+            System.out.print("*");
+            System.out.println();
+        }
+
+    }
+}

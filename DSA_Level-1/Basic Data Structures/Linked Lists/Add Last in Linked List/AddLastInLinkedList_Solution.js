c993b4d572caf7c650221099b2bedfab48a617ac
Warun Sharma <warunsharma.95@gmail.com>
2021-12-16 21:15:54 +0530

    Added solution for Add element in Last in Linked List

---
+++ DSA_Level-1/Basic Data Structures/Linked Lists/Add Last in Linked List/AddLastInLinkedList_Solution.js
@@ -0,0 +1,40 @@
+class Node{
+    constructor(data){
+        this.data=data;
+        this.next=null;
+    }
+}
+
+class LinkedList{
+    head=null;
+    tail=null;
+    size=0;
+
+    length(){
+        return this.size;
+    }
+
+    addLast(val){
+        let temp=new Node(val);
+        if(this.head==null){
+            this.head=this.tail=temp;
+        }
+        else{
+            this.tail.next=temp;
+            this.tail=temp;
+        }
+        ++this.size;
+    }
+
+}
+
+let ll1=new LinkedList();
+ll1.addLast(1);
+ll1.addLast(2);
+ll1.addLast(3);
+ll1.diplay();
+console.log(ll1.length());
+ll1.addLast(4);
+ll1.addLast(5);
+ll1.diplay();
+console.log(ll1.length());

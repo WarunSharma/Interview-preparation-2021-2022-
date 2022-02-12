3eee51228b995f977885a378a94576a810d44abf
Warun Sharma <warunsharma.95@gmail.com>
2022-02-12 19:01:34 +0530

    Added solution for Implement a Priority Queue & K Largest Element problems

---
+++ DSA_Level-1/Advanced Data Structure/Hashmap And Heap/K Largest Elements/KLargestElements_Solution.java
@@ -0,0 +1,36 @@
+import java.util.*;
+ import java.lang.*;
+ import java.io.*;
+ class Main
+  {
+ 	 public static void main (String[] args) throws IOException
+ 	  {
+ 	 	 Scanner scn= new Scanner(System.in);
+ 	    	 int n = scn.nextInt();
+ 	 	 int[] num = new int[n];
+ 	 	 for (int i = 0; i < n; i++) {
+ 	 	 	 	 num[i] = scn.nextInt();
+ 	 	 	 }
+ 	 	 int k = scn.nextInt();
+ 	 	 solve(n,num,k);
+ 	  }
+ 	 // -----------------------------------------------------
+ 	 // This is a functional problem. Only this function has to be written.
+ 	 // This function takes as input an array,n length of array and k.
+ 	 // It should print required output.
+ 	  public static void solve(int n,int[] arr,int k){
+		 //System.out.println("Hello");  
+ 	     PriorityQueue<Integer> pq=new PriorityQueue<>(Collections.reverseOrder());
+		 
+		 for(int a:arr){
+			 //System.out.println(a);  
+			 pq.add(a);
+		 } 
+		 //System.out.println("Hello");	
+		 while(pq.size()>0 && k>0){
+			 System.out.print(pq.peek()+" ");
+			 pq.remove();
+			 k--;
+		 }
+     }
+ }
---
+++ DSA_Level-1/Advanced Data Structure/Hashmap And Heap/K Largest Elements/KLargestElements_Solution.js
@@ -0,0 +1,126 @@
+
+class PriorityQueue{
+    constructor(options){
+        this.data=[];
+        this.options=options
+        console.log(options);
+    }
+
+    peek(){
+        if(this.data.length==0){
+            console.log("Underflow");
+            return -1;
+        }
+
+        return this.data[0];
+    }
+
+    swap(pi,idx){
+        let temp=this.data[idx];
+        this.data[idx]=this.data[pi];
+        this.data[pi]=temp;
+    }
+
+    upHeapify(idx){
+        if(idx==0) 
+            return;
+
+        let pi=Math.ceil((idx-1)/2);
+        // console.log(pi);
+        if(this.options==undefined){
+            if(this.data[pi]>this.data[idx]){
+            this.swap(pi,idx);
+            this.upHeapify(pi);
+            }
+        }
+        else{
+            if(this.data[pi]<this.data[idx]){
+            this.swap(pi,idx);
+            this.upHeapify(pi);
+            }
+        }
+    }
+
+    add(val){
+        this.data.push(val);
+        this.upHeapify(this.data.length-1);
+        // if(options==undefined){
+        //     this.data.push(val);
+        //     this.upHeapify(this.data.length-1);
+        // }
+        // else{
+        //     this.data.push(val);
+        //     this.upHeapify(this.data.length-1);
+        // }
+    }
+
+    downHeapify(idx){
+        let min=idx;
+        let max=idx
+        let lci=(2*idx)+1;
+        let rci=(2*idx)+2;
+
+        if(this.options==undefined){
+            if(lci<=this.data.length-1 && this.data[min]>this.data[lci]){
+                min=lci;
+            }
+
+            if(rci<=this.data.length-1 && this.data[min]>this.data[rci]){
+                min=rci;
+            }
+
+            if(min!=idx){
+            this.swap(idx,min);
+            this.downHeapify(min);
+            }
+        }
+        else{
+            if(lci<=this.data.length-1 && this.data[max]<this.data[lci]){
+                max=lci;
+            }
+
+            if(rci<=this.data.length-1 && this.data[max]<this.data[rci]){
+                max=rci;
+            }
+
+            if(max!=idx){
+            this.swap(idx,max);
+            this.downHeapify(max);
+            }
+        }
+
+    }
+    remove(){
+        if(this.data.length==0){
+            console.log("underflow");
+            return -1;
+        }
+
+        this.swap(0,this.data.length-1);
+        let val=this.data.pop();
+        this.downHeapify(0);
+        return val;
+    }
+
+    display(){
+        console.log(this.data);
+    }
+
+    size(){
+        return this.data.length;
+    }
+}
+
+
+let pq=new PriorityQueue({reverse:true});
+let arr=[10,19,3,74,86,57,24,5,11];
+let k=3;
+
+for(let a of arr){
+    pq.add(a);
+}
+
+pq.display();
+
+while(pq.size()>0 && k--)
+    console.log(pq.remove());
